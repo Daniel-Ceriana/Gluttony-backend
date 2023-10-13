df227eafada4cb5b008fe45be36fc011d20f064a
Daniel Ceriana <danielcerianax@gmail.com>
2023-10-12 22:34:44 -0300

    cambiados los returns en controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,74 +10,123 @@ let options = {
   // amount of elements per page
   limit: 2,
 };
+let notFound = { success: true, message: "Product/ s not found" };
 
 const productsController = {
   getProductById: async (req, res) => {
+    let status;
+    let response;
     let product;
     const id = req.params.id;
     try {
       product = await Products.findOne({ _id: id });
-      return res.status(200).json({ success: true, product: product });
+      status = 200;
+      console.log(product);
+      if (product != null) {
+        response = { success: true, product: product };
+      } else {
+        response = notFound;
+      }
     } catch (err) {
-      res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = {
+        success: false,
+        error: err,
+      };
     }
-    res.json({ product });
+    return res.status(status).json(response);
   },
   getProducts: async (req, res) => {
+    let status;
+    let response;
     let products;
     let query = {};
     console.log(req.query);
 
     // multiple queries
     if (req.query.categorie) {
-      query = { ...query, categorie: req.query.categorie };
+      query.categorie = req.query.categorie;
     }
     if (req.query._id) {
-      query = { ...query, _id: req.query._id };
+      query._id = req.query._id;
     }
     if (req.query.name) {
-      query = { ...query, name: { $in: new RegExp("^" + req.query.name) } };
+      const auxQuery = req.query.name.replace("_", " ");
+      query.name = { $in: new RegExp("^" + auxQuery) };
     }
-
     try {
       products = await Products.find(query);
-      return res.status(200).json({ success: true, products: products });
+      console.log(products);
+      if (products.length == 0) {
+        status = 404;
+        response = { success: true, message: "No hay productos" };
+      } else {
+        status = 200;
+        response = { success: true, products: products };
+      }
+      // return res.status(200).json({ success: true, products: products });
     } catch (err) {
-      return res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = { success: false, error: err };
+      // chequear que esto sea correcto en buenas practicas
+      // si hay error habria que manejarlo inmediatamente
+      // return res.status(500).json({ success: false, error: err });
     }
+    return res.status(status).json(response);
   },
   createProduct: async (req, res) => {
+    let status;
+    let response;
     let auxProduct;
     try {
       auxProduct = await Products.create(req.body);
-      return res.status(201).json({ success: true, product: auxProduct });
+      status = 201;
+      response = { success: true, product: auxProduct };
+      // return res.status(201).json({ success: true, product: auxProduct });
     } catch (err) {
-      return res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = { success: false, error: err };
+      // return res.status(500).json({ success: false, error: err });
     }
+    return res.status(status).json(response);
   },
   updateProduct: async (req, res) => {
+    let status;
+    let response;
     try {
       const product = await Products.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         { new: true }
       );
-      return res.status(200).json({ success: true, product: product });
+      status = 200;
+      response = { success: true, product: product };
+      // return res.status(200).json({ success: true, product: product });
     } catch (err) {
-      return res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = { success: false, error: err };
     }
+    return res.status(status).json(response);
   },
   deleteProduct: async (req, res) => {
+    let status;
+    let response;
     try {
       await Products.findByIdAndDelete({ _id: req.params.id });
-      return res
-        .status(200)
-        .json({ success: true, message: "Producto eliminado" });
+      status = 200;
+      response = { success: true, message: "Producto eliminado" };
+      // return res
+      // .status(200)
+      // .json({ success: true, message: "Producto eliminado" });
     } catch (err) {
-      return res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = { success: false, error: err };
     }
+    return res.status(status).json(response);
   },
   getPagination: async (req, res) => {
+    let status;
+    let response;
     let products;
     try {
       let query = req.query.page;
@@ -87,10 +136,13 @@ const productsController = {
         options.page = query;
       }
       products = await Products.paginate({}, options);
-      return res.status(200).json({ success: true, products: products });
+      status = 200;
+      response = { success: true, product: product };
     } catch (err) {
-      return res.status(500).json({ success: false, error: err });
+      status = 500;
+      response = { success: false, error: err };
     }
+    return res.status(status).json(response);
   },
 };
 
