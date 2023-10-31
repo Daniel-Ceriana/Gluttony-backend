426e689b574ec9f16c2572a8150ab76ad479011e
Daniel Ceriana <danielcerianax@gmail.com>
2023-10-31 11:28:37 -0300

    primeros cambios para email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,172 +1,173 @@
 const User = require("../models/userModel.js");
 const bcryptjs = require("bcryptjs");
-
+const crypto = require("crypto");
 //
 
 const userController = {
-  signIn: async (req, res) => {
-    const { email, password, from } = req.body.userData;
+    signIn: async(req, res) => {
+        const { email, password, from } = req.body.userData;
 
-    try {
-      const user = await User.findOne({ email });
+        try {
+            const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.json({
-          success: false,
-          from: from,
-          message: "Please sign up before signing in.",
-        });
-      }
+            if (!user) {
+                return res.json({
+                    success: false,
+                    from: from,
+                    message: "Please sign up before signing in.",
+                });
+            }
 
-      const isPasswordCorrect = user.password.filter((pass) =>
-        bcryptjs.compareSync(password, pass)
-      );
-      console.log(isPasswordCorrect);
-      console.log(bcryptjs.compareSync(password, user.password[0]));
-      const dataUser = {
-        id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        from: from,
-      };
+            const isPasswordCorrect = user.password.filter((pass) =>
+                bcryptjs.compareSync(password, pass)
+            );
+            console.log(isPasswordCorrect);
+            console.log(bcryptjs.compareSync(password, user.password[0]));
+            const dataUser = {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                from: from,
+            };
 
-      if (from !== "signUp-form") {
-        if (isPasswordCorrect.length > 0) {
-          const hashPassword = bcryptjs.hashSync(password, 10);
-          user.from.push(from);
-          user.password.push(hashPassword);
+            if (from !== "signUp-form") {
+                if (isPasswordCorrect.length > 0) {
+                    const hashPassword = bcryptjs.hashSync(password, 10);
+                    user.from.push(from);
+                    user.password.push(hashPassword);
 
-          await user.save();
+                    await user.save();
 
-          res.json({
-            success: true,
-            from,
-            response: { dataUser },
-            message: from + " added to your signing in methods",
-          });
-        } else {
-          res.json({
-            success: false,
-            from,
-            message: "User or password incorrect",
-          });
+                    res.json({
+                        success: true,
+                        from,
+                        response: { dataUser },
+                        message: from + " added to your signing in methods",
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        from,
+                        message: "User or password incorrect",
+                    });
+                }
+            } else {
+                if (isPasswordCorrect.length > 0) {
+                    res.json({
+                        success: true,
+                        from,
+                        response: { dataUser },
+                        message: "Welcome back, " + dataUser.fullName,
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        from,
+                        message: "User or password incorrect",
+                    });
+                }
+            }
+        } catch (err) {
+            res.json({
+                success: false,
+                from: from,
+                message: "Ups, something went wrong, please try again in a few minutes",
+                response: err,
+            });
         }
-      } else {
-        if (isPasswordCorrect.length > 0) {
-          res.json({
-            success: true,
-            from,
-            response: { dataUser },
-            message: "Welcome back, " + dataUser.fullName,
-          });
-        } else {
-          res.json({
-            success: false,
-            from,
-            message: "User or password incorrect",
-          });
+    },
+    signUp: async(req, res) => {
+        // faltan validaciones
+        const userDataNames = [
+            "fullName",
+            "dni",
+            "email",
+            "password",
+            "from",
+            "aplication",
+        ];
+        const emailVerify = false;
+        const uniqueString = crypto.randomBytes(15).toString("hex");
+        if (!req.body.userData) {
+            return res.json({
+                success: false,
+                message: "Error: no data found",
+            });
         }
-      }
-    } catch (err) {
-      res.json({
-        success: false,
-        from: from,
-        message: "Ups, something went wrong, please try again in a few minutes",
-        response: err,
-      });
-    }
-  },
-  signUp: async (req, res) => {
-    // faltan validaciones
-    const userDataNames = [
-      "fullName",
-      "dni",
-      "email",
-      "password",
-      "from",
-      "aplication",
-    ];
-    if (!req.body.userData) {
-      return res.json({
-        success: false,
-        message: "Error: no data found",
-      });
-    }
-    // checks if something's missing in req.body.userData
-    for (let i = 0; i < userDataNames.length; i++) {
-      if (!req.body.userData[userDataNames[i]]) {
-        return res.json({
-          success: false,
-          message: "Error: no data (" + userDataNames[i] + ") found",
-        });
-      }
-    }
-
-    const { fullName, dni, email, password, from, aplication } =
-      req.body.userData;
-    const hashPassword = bcryptjs.hashSync(password, 10);
-    try {
-      const userExist = await User.findOne({ email });
-
-      if (userExist) {
-        if (userExist.from.indexOf(from) !== -1) {
-          res.json({
-            success: false,
-            from: from,
-            message: "You already have an account, please, sign in instead.",
-          });
-        } else {
-          userExist.from.push(from);
-          userExist.password.push(hashPassword);
-
-          await userExist.save();
-
-          res.json({
-            success: true,
-            from: from,
-            message: from + " was added to your sign in methods.",
-          });
+        // checks if something's missing in req.body.userData
+        for (let i = 0; i < userDataNames.length; i++) {
+            if (!req.body.userData[userDataNames[i]]) {
+                return res.json({
+                    success: false,
+                    message: "Error: no data (" + userDataNames[i] + ") found",
+                });
+            }
         }
-      } else {
-        const newUser = new User({
-          fullName,
-          dni,
-          email,
-          password: [hashPassword],
-          from: [from],
-          aplication,
-        });
 
-        await newUser.save();
+        const { fullName, dni, email, password, from, aplication } =
+        req.body.userData;
+        const hashPassword = bcryptjs.hashSync(password, 10);
+        try {
+            const userExist = await User.findOne({ email });
 
-        res.json({
-          success: true,
-          from: from,
-          message:
-            "User created and added " + from + " to your sign in methods",
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      res.json({
-        success: false,
-        message: "something's gone wrong, try again in a few minutes",
-      });
-    }
-  },
-  updateUser: async (req, res) => {
-    try {
-      // sacar el id desde el token
-      const user = await User.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        { new: true }
-      );
-      return res.status(200).json({ success: true, user: user });
-    } catch (err) {
-      return res.status(500).json({ success: false, error: err });
-    }
-  },
+            if (userExist) {
+                if (userExist.from.indexOf(from) !== -1) {
+                    res.json({
+                        success: false,
+                        from: from,
+                        message: "You already have an account, please, sign in instead.",
+                    });
+                } else {
+                    userExist.from.push(from);
+                    userExist.password.push(hashPassword);
+
+                    await userExist.save();
+
+                    res.json({
+                        success: true,
+                        from: from,
+                        message: from + " was added to your sign in methods.",
+                    });
+                }
+            } else {
+                const newUser = new User({
+                    fullName,
+                    dni,
+                    email,
+                    password: [hashPassword],
+                    from: [from],
+                    aplication,
+                    uniqueString,
+                    emailVerify,
+                });
+
+                await newUser.save();
+
+                res.json({
+                    success: true,
+                    from: from,
+                    message: "User created and added " + from + " to your sign in methods",
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            res.json({
+                success: false,
+                message: "something's gone wrong, try again in a few minutes",
+            });
+        }
+    },
+    updateUser: async(req, res) => {
+        try {
+            // sacar el id desde el token
+            const user = await User.findOneAndUpdate({ _id: req.params.id },
+                req.body, { new: true }
+            );
+            return res.status(200).json({ success: true, user: user });
+        } catch (err) {
+            return res.status(500).json({ success: false, error: err });
+        }
+    },
 };
 
 module.exports = userController;
--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,26 @@
+const { boolean } = require("joi");
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  dni: { type: String, required: true },
-  email: { type: String, required: true },
-  password: [{ type: String, required: true }],
-  from: { type: Array },
-  aplication: { type: String },
+    fullName: { type: String, required: true },
+    dni: { type: String, required: true },
+    email: { type: String, required: true },
+    password: [{ type: String, required: true }],
+    from: { type: Array },
+    aplication: { type: String },
+    uniqueString: { type: String, required: true },
+    emailVerify: { type: Boolean, required: true },
 
-  // cellphone: { type: String, required: false },
-  // street: { type: String, required: false },
-  // city: { type: Number, required: false, default: "" },
-  // state: { type: String, required: false },
-  // postalCode: { type: Number, required: false },
-  // country: { type: String, required: false },
+    // cellphone: { type: String, required: false },
+    // street: { type: String, required: false },
+    // city: { type: Number, required: false, default: "" },
+    // state: { type: String, required: false },
+    // postalCode: { type: Number, required: false },
+    // country: { type: String, required: false },
 
-  //   cart: { type: Array, required: false },
-  // objeto carrito
-  //   buyhistory: { type: String, required: false },
-  // productos/fechas/etc
+    //   cart: { type: Array, required: false },
+    // objeto carrito
+    //   buyhistory: { type: String, required: false },
+    // productos/fechas/etc
 });
 
 const Users = mongoose.model("user", userSchema);
