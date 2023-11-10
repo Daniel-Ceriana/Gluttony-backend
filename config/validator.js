b3becc6b987517d3606b5760f67f5170f2f6f299
Daniel Ceriana <danielcerianax@gmail.com>
2023-11-10 10:33:17 -0300

    actualizadas credenciales para mail

--- config/validator.js
+++ config/validator.js
@@ -1,58 +1,59 @@
 const joi = require("joi");
 
 const signUpValidator = (req, res, next) => {
-    const schema = joi.object({
-        fullName: joi.string().required(),
-        email: joi.string().email({ minDomainSegments: 2 }).required().messages({
-            "string.email": "wrong email format",
-        }),
-        dni: joi.number().required(),
-        password: joi
-            .string()
-            .min(8)
-            .max(30)
-            .pattern(new RegExp(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/))
-            .required()
-            .messages({
-                "string.min": "password must have a minimum of 8 characters debe",
-                "string.max": "password must have a maximum of 30 characters",
-                "string.pattern.base": "password must have at least one uppercase, lowercase and number",
-            }),
-        from: joi.string().required(),
-        aplication: joi.string().required(),
+  const schema = joi.object({
+    fullName: joi.string().required(),
+    email: joi.string().email({ minDomainSegments: 2 }).required().messages({
+      "string.email": "wrong email format",
+    }),
+    // dni: joi.number().required(),
+    password: joi
+      .string()
+      .min(8)
+      .max(30)
+      .pattern(new RegExp(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/))
+      .required()
+      .messages({
+        "string.min": "password must have a minimum of 8 characters debe",
+        "string.max": "password must have a maximum of 30 characters",
+        "string.pattern.base":
+          "password must have at least one uppercase, lowercase and number",
+      }),
+    from: joi.string().required(),
+    aplication: joi.string().required(),
+  });
+  const validation = schema.validate(req.body.userData, { abortEarly: false });
+  if (validation.error) {
+    return res.json({
+      success: false,
+      from: "validator",
+      message: validation.error.details,
     });
-    const validation = schema.validate(req.body.userData, { abortEarly: false });
-    if (validation.error) {
-        return res.json({
-            success: false,
-            from: "validator",
-            message: validation.error.details,
-        });
-    }
+  }
 
-    next();
+  next();
 };
 
 const signInValidator = (req, res, next) => {
-    // before checking data values, checks if userData exists
-    const schema = joi.object({
-        email: joi.string().email({ minDomainSegments: 2 }).required().messages({
-            "string.email": "wrong email format",
-        }),
-        password: joi.string().required(),
-        //   agregar los required del from y aplication
-        from: joi.string(),
-        aplication: joi.string(),
+  // before checking data values, checks if userData exists
+  const schema = joi.object({
+    email: joi.string().email({ minDomainSegments: 2 }).required().messages({
+      "string.email": "wrong email format",
+    }),
+    password: joi.string().required(),
+    //   agregar los required del from y aplication
+    from: joi.string(),
+    aplication: joi.string(),
+  });
+  const validation = schema.validate(req.body.userData, { abortEarly: false });
+  if (validation.error) {
+    return res.json({
+      success: false,
+      from: "validator",
+      message: validation.error.details,
     });
-    const validation = schema.validate(req.body.userData, { abortEarly: false });
-    if (validation.error) {
-        return res.json({
-            success: false,
-            from: "validator",
-            message: validation.error.details,
-        });
-    }
+  }
 
-    next();
+  next();
 };
 module.exports = { signUpValidator, signInValidator };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const { signUpValidator, signInValidator } = require("../config/validator");
 const userController = require("../controllers/userController");
 
 userRouter.post("/auth/signUp", signUpValidator, userController.signUp);
-userRouter.get("/auth/signIn", signInValidator, userController.signIn);
+userRouter.post("/auth/signIn", signInValidator, userController.signIn);
 userRouter.get("/auth/verifyEmail/:string", userController.verifyUserAccount);
 // userRouter.put("/:id", userController.updateUser);
 // -----------------------token
